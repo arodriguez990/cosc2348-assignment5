1c84f9b56166da8dbd5374993459e57edad0ad60
arodriguez <arodriguez@Ubuntu22.10.myguest.virtualbox.org>
2026-04-19 15:20:26 -0500

    Added Makefile, github.txt, and updated your_tests.py

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -4,6 +4,11 @@ from calculator_adapter import run
 
 ### ADD AT LEAST TWO TESTS HERE!
 
+# Checks that the program outputs "4" for an input of "2 + 2"
+assert run("2 + 2").output == "4"
+
+# Checks that the program outputs "9" for an input of "3 * 3"
+assert run("3 * 3").output == "9"
 
 ###
 
